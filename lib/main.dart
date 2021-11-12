a0f797b68a2bf538c4288aaf0b9578877ccafca0
azarath1 <ar2oww@gmail.com>
2021-11-12 08:58:44 +0100

    feat: add colorful gradient animated background

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,72 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HaiQ',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(),
+      home: const AnimatedGradient(title: "HaiQ"),
     );
   }
 }
 
+class AnimatedGradient extends StatefulWidget {
+  const AnimatedGradient({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _AnimatedGradientState createState() => _AnimatedGradientState();
+}
+class _AnimatedGradientState extends State<AnimatedGradient> {
+  List<Color> colorList = [
+    Colors.amber,
+    Colors.cyanAccent,
+    Colors.lightGreen,
+    Colors.red
+  ];
+  List<Alignment> alignmentList = [
+    Alignment.bottomLeft,
+    Alignment.bottomRight,
+    Alignment.topRight,
+    Alignment.topLeft,
+  ];
+  int index = 0;
+  Color bottomColor = Colors.pinkAccent;
+  Color topColor = Colors.orange;
+  Alignment begin = Alignment.bottomLeft;
+  Alignment end = Alignment.topRight;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+          children: [
+            AnimatedContainer(
+              duration: Duration(seconds: 2),
+              onEnd: () {
+                setState(() {
+                  index = index + 1;
+                  // animate the color
+                  bottomColor = colorList[index % colorList.length];
+                  topColor = colorList[(index + 1) % colorList.length];
+
+                  //// animate the alignment
+                  // begin = alignmentList[index % alignmentList.length];
+                  // end = alignmentList[(index + 2) % alignmentList.length];
+                });
+              },
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: begin, end: end, colors: [bottomColor, topColor])),
+            ),
+            Positioned.fill(
+              child: IconButton(
+                icon: Icon(Icons.play_arrow),
+                onPressed: () {
+                  setState(() {
+                    bottomColor = Colors.red;
+                  });
+                },
+              ),
+            )
+          ],
+        ));
+  }
+}
